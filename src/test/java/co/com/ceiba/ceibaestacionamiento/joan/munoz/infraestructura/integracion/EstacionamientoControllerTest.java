5b4a15dd1ce0c93512b4325543511b6e32ac7c29
joan.munoz <josemuba94@gmail.com>
2019-06-12 07:57:21 -0500

    Se modifica la dirección del proyecto en el sonar.properties.

--- src/test/java/co/com/ceiba/ceibaestacionamiento/joan/munoz/infraestructura/integracion/EstacionamientoControllerTest.java
+++ src/test/java/co/com/ceiba/ceibaestacionamiento/joan/munoz/infraestructura/integracion/EstacionamientoControllerTest.java
@@ -27,6 +27,7 @@ public class EstacionamientoControllerTest {
 
 	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
 			MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+	
 	@Autowired
 	private RepositorioRegistroParqueo repositorioRegistroParqueo;
 	@Autowired
