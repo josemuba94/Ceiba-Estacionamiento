ecb7487c90449733d6d3319d5fbed8def20825e4
joan.munoz <josemuba94@gmail.com>
2019-06-17 11:47:44 -0500

    Se modifica la salida para la consulta de vehículos ingresados en el estacionamiento, para que solo proyecte la placa, el tipo de vehículo y la fecha de ingreso.

--- src/main/java/co/com/ceiba/ceibaestacionamiento/joan/munoz/aplicacion/dtos/EstadoEstacionamientoDTO.java
+++ src/main/java/co/com/ceiba/ceibaestacionamiento/joan/munoz/aplicacion/dtos/EstadoEstacionamientoDTO.java
@@ -1,5 +1,6 @@
 package co.com.ceiba.ceibaestacionamiento.joan.munoz.aplicacion.dtos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import co.com.ceiba.ceibaestacionamiento.joan.munoz.dominio.modelo.RegistroParqueo;
@@ -9,10 +10,16 @@ import lombok.Getter;
 public class EstadoEstacionamientoDTO {
 
 	private List<String> tiposVehiculo;
-	private List<RegistroParqueo> vehiculosIngresados;
-	
+	private List<VehiculoIngresadoDTO> vehiculosIngresados;
+
 	public EstadoEstacionamientoDTO(List<String> tiposVehiculo, List<RegistroParqueo> vehiculosIngresados) {
 		this.tiposVehiculo = tiposVehiculo;
-		this.vehiculosIngresados = vehiculosIngresados;
+		this.vehiculosIngresados = new ArrayList<>();
+		mapearVehiculosIngresados(vehiculosIngresados);
+	}
+
+	public void mapearVehiculosIngresados(List<RegistroParqueo> vehiculosIngresados) {
+		for (int i = 0; i < vehiculosIngresados.size(); i++)
+			this.vehiculosIngresados.add(new VehiculoIngresadoDTO(vehiculosIngresados.get(i).getPlaca(), vehiculosIngresados.get(i).getTipoVehiculo(), vehiculosIngresados.get(i).getFechaIngreso()));
 	}
 }
